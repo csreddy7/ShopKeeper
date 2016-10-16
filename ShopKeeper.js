0bb226aeb8de94e5bf1b16ff616a46a2b84948d3
csreddy7 <csredde@gmail.com>
2016-10-16 17:24:26 +0530

    alinged login columns center,started using require js for amd

--- ShopKeeper.js
+++ ShopKeeper.js
@@ -1,4 +1,18 @@
-var ShopKeeper={
-	title:"ShopKeeper"
-};
+
+$('document').ready(function(){
+		setTimeout(function(){
+			$('<sub>hop</sub>').appendTo('.logo span:first-child');
+			$('<sub>eeper</sub>').appendTo('.logo span:last-child');
+		},2000);
+	});
+
+/*define(["node_modules/jquery/dist/jquery"],function($){
+	$('document').ready(function(){
+		setTimeout(function(){
+			$('<sub>hop</sub>').appendTo('.logo span:first-child');
+			$('<sub>eeper</sub>').appendTo('.logo span:last-child');
+		},2000);
+	});
+});
+*/
 
